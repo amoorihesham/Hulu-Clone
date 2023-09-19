f6cdff23dceba4fc03a1905bf9509d3967ea7797
Amr Hesham <msam.team20@gmail.com>
2023-09-19 21:54:31 +0300

    solve api key

--- components/Results.js
+++ components/Results.js
@@ -4,14 +4,14 @@ import FlipMove from "react-flip-move";
 function Results({ movies }) {
   const allMovies = movies.results;
   return (
-    <FlipMove
+    <div
       className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 
     3xl:grid-cols-4"
     >
-      {allMovies.map((movie) => (
+      {allMovies?.map((movie) => (
         <Thumbnail key={movie.id} movies={movie} />
       ))}
-    </FlipMove>
+    </div>
   );
 }
 
